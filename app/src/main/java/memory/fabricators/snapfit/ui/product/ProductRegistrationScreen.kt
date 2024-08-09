4b36ee0a4ad856dba7bef5c87c1bc5ab649a1398
junsuPark <krpjs0508@gmail.com>
2024-08-10 00:46:38 +0900

    Product Registration

--- app/src/main/java/memory/fabricators/snapfit/ui/product/ProductRegistrationScreen.kt
+++ app/src/main/java/memory/fabricators/snapfit/ui/product/ProductRegistrationScreen.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -28,9 +30,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import memory.fabricators.snapfit.core.design_system.Button
 import memory.fabricators.snapfit.core.design_system.LocalColorScheme
 import memory.fabricators.snapfit.core.design_system.LocalTypography
 import memory.fabricators.snapfit.core.design_system.TextField
+import memory.fabricators.snapfit.ui.common.Filter
+import memory.fabricators.snapfit.ui.common.FilterButton
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -59,7 +64,8 @@ fun ProductRegistrationScreen(
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPaddings),
+                .padding(innerPaddings)
+                .verticalScroll(rememberScrollState()),
             verticalArrangement = Arrangement.spacedBy(32.dp),
         ) {
             Column(
@@ -121,6 +127,114 @@ fun ProductRegistrationScreen(
                     ),
                 )
             }
+
+            Column(
+                modifier = Modifier.padding(horizontal = 16.dp),
+            ) {
+                Text(
+                    text = "스튜디오 유무",
+                    style = LocalTypography.current.body1Semibold,
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                ) {
+                    FilterButton(
+                        filter = Filter(
+                            id = "123",
+                            text = "테스트",
+                        ),
+                    )
+                    FilterButton(
+                        filter = Filter(
+                            id = "123",
+                            text = "테스트",
+                        ),
+                    )
+                }
+            }
+
+            Column(
+                modifier = Modifier.padding(horizontal = 16.dp),
+            ) {
+                Text(
+                    text = "분위기",
+                    style = LocalTypography.current.body1Semibold,
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                ) {
+                    FilterButton(
+                        filter = Filter(
+                            id = "123",
+                            text = "분위기",
+                        ),
+                    )
+                    FilterButton(
+                        filter = Filter(
+                            id = "123",
+                            text = "분아래기",
+                        ),
+                    )
+                }
+            }
+
+            Column(
+                modifier = Modifier.padding(horizontal = 16.dp),
+            ) {
+                Text(
+                    text = "옵션",
+                    style = LocalTypography.current.body1Semibold,
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                TextField(
+                    value = "시간을 선택해주세요",
+                    onValueChange = {},
+                    modifier = Modifier.fillMaxWidth(),
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                TextField(
+                    value = "가격",
+                    onValueChange = {},
+                    modifier = Modifier.fillMaxWidth(),
+                )
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 16.dp),
+                ) {
+                    Text(text = "옵션 추가하기")
+                }
+            }
+
+            Column(
+                modifier = Modifier.padding(horizontal = 16.dp),
+            ) {
+                Text(
+                    text = "인원 추가 비용",
+                    style = LocalTypography.current.body1Semibold,
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                TextField(
+                    value = "가격",
+                    onValueChange = {},
+                    modifier = Modifier.fillMaxWidth(),
+                )
+            }
+        }
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    start = 16.dp,
+                    end = 16.dp,
+                    bottom = 16.dp,
+                ),
+        ) {
+            Text(text = "등록하기")
         }
     }
 }
