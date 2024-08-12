a310755c5190604aee8ab98e714f4a1444015c0d
junsuPark <krpjs0508@gmail.com>
2024-08-12 20:41:43 +0900

    koin

--- app/src/main/java/memory/fabricators/snapfit/SnapfitApp.kt
+++ app/src/main/java/memory/fabricators/snapfit/SnapfitApp.kt
@@ -6,68 +6,43 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import memory.fabricators.snapfit.data.dataModule
-import memory.fabricators.snapfit.database.databaseModule
-import memory.fabricators.snapfit.datastore.dataStoreModule
-import memory.fabricators.snapfit.network.networkModule
 import memory.fabricators.snapfit.ui.artist.details.ArtistDetailsScreen
 import memory.fabricators.snapfit.ui.artist.listfilter.ArtistListFilterScreen
 import memory.fabricators.snapfit.ui.artist.result.BookingCompletionScreen
 import memory.fabricators.snapfit.ui.main.MainScreen
 import memory.fabricators.snapfit.ui.signup.SignUpScreen
 import memory.fabricators.snapfit.ui.start.StartScreen
-import memory.fabricators.snapfit.ui.uiModule
-import org.koin.compose.KoinApplication
-import org.koin.core.module.Module
-import org.koin.dsl.module
 
 @Composable
 fun SnapfitApp() {
-    KoinApplication(
-        application = {
-            modules(snapfitModule)
-        },
+    val navController = rememberNavController()
+    NavHost(
+        modifier = Modifier.fillMaxSize(),
+        navController = navController,
+        startDestination = SnapfitDestinations.MAIN.route,
     ) {
-        val navController = rememberNavController()
-        NavHost(
-            modifier = Modifier.fillMaxSize(),
-            navController = navController,
-            startDestination = SnapfitDestinations.MAIN.route,
-        ) {
-            composable(route = SnapfitDestinations.MAIN.route) {
-                MainScreen()
-            }
-            composable(route = SnapfitDestinations.START.route) {
-                StartScreen()
-            }
-            composable(route = SnapfitDestinations.SIGN_UP.route) {
-                SignUpScreen(
-                    onOpenMain = { /*TODO*/ },
-                )
-            }
-            composable(route = SnapfitDestinations.ARTIST_DETAILS.route) {
-                ArtistDetailsScreen()
-            }
-            composable(route = SnapfitDestinations.BOOKING_COMPLETION.route) {
-                BookingCompletionScreen()
-            }
-            composable(route = SnapfitDestinations.ARTIST_LIST_FILTER.route) {
-                ArtistListFilterScreen()
-            }
+        composable(route = SnapfitDestinations.MAIN.route) {
+            MainScreen()
+        }
+        composable(route = SnapfitDestinations.START.route) {
+            StartScreen()
+        }
+        composable(route = SnapfitDestinations.SIGN_UP.route) {
+            SignUpScreen(
+                onOpenMain = { /*TODO*/ },
+            )
+        }
+        composable(route = SnapfitDestinations.ARTIST_DETAILS.route) {
+            ArtistDetailsScreen()
+        }
+        composable(route = SnapfitDestinations.BOOKING_COMPLETION.route) {
+            BookingCompletionScreen()
+        }
+        composable(route = SnapfitDestinations.ARTIST_LIST_FILTER.route) {
+            ArtistListFilterScreen()
         }
     }
 }
-
-val snapfitModule: Module
-    get() = module {
-        includes(
-            dataModule,
-            databaseModule,
-            dataStoreModule,
-            networkModule,
-            uiModule,
-        )
-    }
 
 enum class SnapfitDestinations(
     val route: String,
--- app/src/main/java/memory/fabricators/snapfit/SnapfitApplication.kt
+++ app/src/main/java/memory/fabricators/snapfit/SnapfitApplication.kt
@@ -2,10 +2,39 @@ package memory.fabricators.snapfit
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
+import memory.fabricators.snapfit.data.dataModule
+import memory.fabricators.snapfit.database.databaseModule
+import memory.fabricators.snapfit.datastore.dataStoreModule
+import memory.fabricators.snapfit.network.networkModule
+import memory.fabricators.snapfit.ui.uiModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.core.module.Module
+import org.koin.dsl.module
 
 class SnapfitApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         KakaoSdk.init(this, BuildConfig.KAKAO_API_KEY)
+
+        startKoin {
+            androidLogger()
+            androidContext(this@SnapfitApplication)
+            modules(
+                snapfitModule,
+            )
+        }
     }
 }
+
+val snapfitModule: Module
+    get() = module {
+        includes(
+            dataModule,
+            databaseModule,
+            dataStoreModule,
+            networkModule,
+            uiModule,
+        )
+    }
