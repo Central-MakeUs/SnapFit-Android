28e341392aa30ba6daf8f1227cdb130a9d1a069a
junsuPark <krpjs0508@gmail.com>
2024-08-20 21:26:33 +0900

    logout

--- app/src/main/java/memory/fabricators/snapfit/network/login/LoginNetworkDataSource.kt
+++ app/src/main/java/memory/fabricators/snapfit/network/login/LoginNetworkDataSource.kt
@@ -5,12 +5,11 @@ import memory.fabricators.snapfit.network.login.model.TokenResponse
 
 abstract class LoginNetworkDataSource {
     abstract suspend fun login(token: String): TokenResponse
-    abstract suspend fun logout()
+    abstract suspend fun logout(token: String)
     abstract suspend fun socialSignUp(
         req: SocialSignUpRequest,
         socialAccessToken: String,
     ): TokenResponse
 
     abstract suspend fun reissueToken(): TokenResponse
-    abstract suspend fun logout(token: String)
 }
