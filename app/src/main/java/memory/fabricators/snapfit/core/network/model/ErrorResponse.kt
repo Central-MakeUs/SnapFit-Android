9fd406f17694042fe257be351d4cbe8d91bfb67d
junsuPark <krpjs0508@gmail.com>
2024-08-20 20:07:32 +0900

    error response

---
+++ app/src/main/java/memory/fabricators/snapfit/core/network/model/ErrorResponse.kt
@@ -0,0 +1,9 @@
+package memory.fabricators.snapfit.core.network.model
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class ErrorResponse(
+    val message: String,
+    val errorCode: Int,
+)
