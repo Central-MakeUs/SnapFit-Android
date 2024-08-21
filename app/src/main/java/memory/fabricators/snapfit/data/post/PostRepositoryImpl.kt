ccb480ba768aca94cbb419c9e750128541ec928a
junsuPark <krpjs0508@gmail.com>
2024-08-21 21:01:26 +0900

    post repo impl

---
+++ app/src/main/java/memory/fabricators/snapfit/data/post/PostRepositoryImpl.kt
@@ -0,0 +1,212 @@
+package memory.fabricators.snapfit.data.post
+
+import memory.fabricators.snapfit.data.post.model.PostDetails
+import memory.fabricators.snapfit.data.post.model.PostList
+import memory.fabricators.snapfit.network.post.PostNetworkDataSource
+import memory.fabricators.snapfit.network.post.model.PostRequest
+
+class PostRepositoryImpl(
+    private val postNetworkDataSource: PostNetworkDataSource,
+) : PostRepository() {
+    override suspend fun getLikedPosts(postId: String): PostList {
+        val response = postNetworkDataSource.getPostLikes(postId)
+        return with(response) {
+            PostList(
+                offset = offset,
+                limit = limit,
+                data = data.map {
+                    PostList.Post(
+                        id = it.id,
+                        maker = PostList.Post.Maker(
+                            id = it.maker.id,
+                            nickname = it.maker.nickname,
+                        ),
+                        title = it.title,
+                        thumbnail = it.thumbnail,
+                        vibes = it.vibes,
+                        locations = it.locations,
+                        price = it.price,
+                        studio = it.studio,
+                        like = it.like,
+                    )
+                },
+            )
+        }
+    }
+
+    override suspend fun getMakerPosts(makerId: String): PostList {
+        val response = postNetworkDataSource.getMakerPosts(makerId)
+        return with(response) {
+            PostList(
+                offset = offset,
+                limit = limit,
+                data = data.map {
+                    PostList.Post(
+                        id = it.id,
+                        maker = PostList.Post.Maker(
+                            id = it.maker.id,
+                            nickname = it.maker.nickname,
+                        ),
+                        title = it.title,
+                        thumbnail = it.thumbnail,
+                        vibes = it.vibes,
+                        locations = it.locations,
+                        price = it.price,
+                        studio = it.studio,
+                        like = it.like,
+                    )
+                },
+            )
+        }
+    }
+
+    override suspend fun getPostDetails(postId: String): PostDetails {
+        val response = postNetworkDataSource.getPostDetails(postId)
+        return with(response) {
+            PostDetails(
+                id = id,
+                maker = PostDetails.Maker(
+                    id = maker.id,
+                    nickname = maker.nickname,
+                ),
+                title = title,
+                thumbnail = thumbnail,
+                images = images,
+                vibes = vibes,
+                locations = locations,
+                studio = studio,
+                like = like,
+                createdAt = createdAt,
+                prices = prices.map {
+                    PostDetails.Price(
+                        min = it.min,
+                        max = it.max,
+                    )
+                },
+                personPrice = personPrice,
+                desc = desc,
+            )
+        }
+    }
+
+    override suspend fun createPost(
+        vibes: List<String>,
+        locations: List<String>,
+        imageNames: List<String>,
+        thumbnail: String,
+        title: String,
+        desc: String,
+        prices: List<Pair<Long, Long>>,
+        personPrice: Long,
+        studio: Boolean,
+    ): PostDetails {
+        val response = postNetworkDataSource.createPost(
+            postRequest = PostRequest(
+                vibes = vibes,
+                locations = locations,
+                imageNames = imageNames,
+                thumbnail = thumbnail,
+                title = title,
+                desc = desc,
+                prices = prices.map {
+                    PostRequest.PriceRequest(
+                        min = it.first,
+                        price = it.second,
+                    )
+                },
+                personPrice = personPrice,
+                studio = studio,
+            )
+        )
+        return with(response) {
+            PostDetails(
+                id = id,
+                maker = PostDetails.Maker(
+                    id = maker.id,
+                    nickname = maker.nickname,
+                ),
+                title = title,
+                thumbnail = thumbnail,
+                images = images,
+                vibes = vibes,
+                locations = locations,
+                studio = studio,
+                like = like,
+                createdAt = createdAt,
+                prices = prices.map {
+                    PostDetails.Price(
+                        min = it.first,
+                        max = it.second,
+                    )
+                },
+                personPrice = personPrice,
+                desc = desc,
+            )
+        }
+    }
+
+    override suspend fun likePost(postId: String) {
+        postNetworkDataSource.likePost(postId)
+    }
+
+    override suspend fun unlikePost(postId: String) {
+        postNetworkDataSource.unlikePost(postId)
+    }
+
+    override suspend fun getPostLikeCount(postId: String): Int {
+        val response = postNetworkDataSource.getPostLikeCount(postId)
+        return response.count
+    }
+
+    override suspend fun getAllPosts(): PostList {
+        val response = postNetworkDataSource.getAllPosts()
+        return with(response) {
+            PostList(
+                offset = offset,
+                limit = limit,
+                data = response.data.map {
+                    PostList.Post(
+                        id = it.id,
+                        maker = PostList.Post.Maker(
+                            id = it.maker.id,
+                            nickname = it.maker.nickname,
+                        ),
+                        title = it.title,
+                        thumbnail = it.thumbnail,
+                        vibes = it.vibes,
+                        locations = it.locations,
+                        price = it.price,
+                        studio = it.studio,
+                        like = it.like,
+                    )
+                },
+            )
+        }
+    }
+
+    override suspend fun getPostsByVibes(vibes: List<String>): PostList {
+        val response = postNetworkDataSource.getPostsByVibes(vibes)
+        return with(response) {
+            PostList(
+                offset = offset,
+                limit = limit,
+                data = data.map {
+                    PostList.Post(
+                        id = it.id,
+                        maker = PostList.Post.Maker(
+                            id = it.maker.id,
+                            nickname = it.maker.nickname,
+                        ),
+                        title = it.title,
+                        thumbnail = it.thumbnail,
+                        vibes = it.vibes,
+                        locations = it.locations,
+                        price = it.price,
+                        studio = it.studio,
+                        like = it.like,
+                    )
+                },
+            )
+        }
+    }
+}
