c3916025b94a071cbd2239f6e5b5d4a0004b831e
junsuPark <krpjs0508@gmail.com>
2024-08-21 20:29:34 +0900

    post

---
+++ app/src/main/java/memory/fabricators/snapfit/network/post/di/PostNetworkModule.kt
@@ -0,0 +1,11 @@
+package memory.fabricators.snapfit.network.post.di
+
+import memory.fabricators.snapfit.network.post.PostNetworkDataSource
+import memory.fabricators.snapfit.network.post.PostNetworkDataSourceImpl
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+val postNetworkModule: Module
+    get() = module {
+        single<PostNetworkDataSource> { PostNetworkDataSourceImpl(httpClient = get()) }
+    }
