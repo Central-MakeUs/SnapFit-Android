3720616bd9a3faee36619ada1b3be6a4b70ab8b5
junsuPark <krpjs0508@gmail.com>
2024-08-21 20:29:48 +0900

    network

--- app/src/main/java/memory/fabricators/snapfit/network/NetworkModule.kt
+++ app/src/main/java/memory/fabricators/snapfit/network/NetworkModule.kt
@@ -1,14 +1,16 @@
 package memory.fabricators.snapfit.network
 
-import memory.fabricators.snapfit.network.user.di.userNetworkModule
 import memory.fabricators.snapfit.network.login.di.loginNetworkModule
+import memory.fabricators.snapfit.network.post.di.postNetworkModule
+import memory.fabricators.snapfit.network.user.di.userNetworkModule
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val networkModule: Module
     get() = module {
         includes(
-            userNetworkModule,
             loginNetworkModule,
+            postNetworkModule,
+            userNetworkModule,
         )
     }
