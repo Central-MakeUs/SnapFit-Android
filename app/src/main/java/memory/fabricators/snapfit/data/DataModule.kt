ee7b0f2052db5ceb71222456b7b3c25e8c55eefc
junsuPark <krpjs0508@gmail.com>
2024-08-21 21:01:35 +0900

    data

--- app/src/main/java/memory/fabricators/snapfit/data/DataModule.kt
+++ app/src/main/java/memory/fabricators/snapfit/data/DataModule.kt
@@ -1,14 +1,16 @@
 package memory.fabricators.snapfit.data
 
-import memory.fabricators.snapfit.data.user.di.userDataModule
 import memory.fabricators.snapfit.data.login.di.loginDataModule
+import memory.fabricators.snapfit.data.post.di.postDataModule
+import memory.fabricators.snapfit.data.user.di.userDataModule
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val dataModule: Module
     get() = module {
         includes(
-            userDataModule,
             loginDataModule,
+            postDataModule,
+            userDataModule,
         )
     }
