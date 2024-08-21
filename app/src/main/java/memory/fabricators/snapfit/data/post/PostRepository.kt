2a90756a311867c8575a5d2895949f0765184879
junsuPark <krpjs0508@gmail.com>
2024-08-21 20:46:39 +0900

    post repo

---
+++ app/src/main/java/memory/fabricators/snapfit/data/post/PostRepository.kt
@@ -0,0 +1,27 @@
+package memory.fabricators.snapfit.data.post
+
+import memory.fabricators.snapfit.data.post.model.PostDetails
+import memory.fabricators.snapfit.data.post.model.PostList
+
+abstract class PostRepository {
+    abstract suspend fun getLikedPosts(postId: String): PostList
+    abstract suspend fun getMakerPosts(makerId: String): PostList
+    abstract suspend fun getPostDetails(postId: String): PostDetails
+    abstract suspend fun createPost(
+        vibes: List<String>,
+        locations: List<String>,
+        imageNames: List<String>,
+        thumbnail: String,
+        title: String,
+        desc: String,
+        prices: List<Pair<Long, Long>>,
+        personPrice: Long,
+        studio: Boolean,
+    ): PostDetails
+
+    abstract suspend fun likePost(postId: String)
+    abstract suspend fun unlikePost(postId: String)
+    abstract suspend fun getPostLikeCount(postId: String): Int
+    abstract suspend fun getAllPosts(): PostList
+    abstract suspend fun getPostsByVibes(vibes: List<String>): PostList
+}
