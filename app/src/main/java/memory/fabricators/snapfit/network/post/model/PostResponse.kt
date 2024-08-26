51b6d3314c9ff2abc290535750e8d307fb4560a4
junsuPark <krpjs0508@gmail.com>
2024-08-26 20:42:56 +0900

    post

--- app/src/main/java/memory/fabricators/snapfit/network/post/model/PostResponse.kt
+++ app/src/main/java/memory/fabricators/snapfit/network/post/model/PostResponse.kt
@@ -9,7 +9,7 @@ data class PostResponse(
     val maker: MakerResponse,
     val createdAt: String,
     val thumbnail: String,
-    val images: String,
+    val images: List<String>,
     val desc: String,
     val title: String,
     val vibes: List<String>,
