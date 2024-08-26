a107ade978ab236821afa959f66c6b15d13feadb
junsuPark <krpjs0508@gmail.com>
2024-08-26 21:21:03 +0900

    vm

--- app/src/main/java/memory/fabricators/snapfit/ui/post/details/PostDetailsViewModel.kt
+++ app/src/main/java/memory/fabricators/snapfit/ui/post/details/PostDetailsViewModel.kt
@@ -6,6 +6,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import memory.fabricators.snapfit.data.post.PostRepository
 import memory.fabricators.snapfit.data.post.model.PostDetails
+import memory.fabricators.snapfit.data.post.model.PostList
 import org.orbitmvi.orbit.ContainerHost
 import org.orbitmvi.orbit.syntax.simple.intent
 import org.orbitmvi.orbit.syntax.simple.reduce
@@ -21,9 +22,27 @@ class PostDetailsViewModel(
             kotlin.runCatching {
                 postRepository.getPostDetails(postId)
             }.onSuccess {
-                println("POSTPOST $it")
+                fetchArtistPost(makerId = it.maker.id)
                 reduce {
                     state.copy(postDetails = it)
+                }
+            }.onFailure {
+                it.printStackTrace()
+            }
+        }
+    }
+
+    private fun fetchArtistPost(makerId: Long) = intent {
+        viewModelScope.launch(Dispatchers.IO) {
+            kotlin.runCatching {
+                postRepository.getMakerPosts(
+                    makerId = makerId,
+                    offset = 0,
+                    limit = 2,
+                )
+            }.onSuccess {
+                reduce {
+                    state.copy(artistPosts = it.data)
                 }
             }.onFailure {
                 it.printStackTrace()
@@ -34,4 +53,5 @@ class PostDetailsViewModel(
 
 data class PostDetailsState(
     val postDetails: PostDetails? = null,
+    val artistPosts: List<PostList.Post>? = null,
 )
