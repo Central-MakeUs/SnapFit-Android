22a1962e7cf943cf9b9e8e0e539619405cba30ff
junsuPark <krpjs0508@gmail.com>
2024-08-26 20:19:26 +0900

    mod

--- app/src/main/java/memory/fabricators/snapfit/ui/UiModule.kt
+++ app/src/main/java/memory/fabricators/snapfit/ui/UiModule.kt
@@ -2,6 +2,7 @@ package memory.fabricators.snapfit.ui
 
 import memory.fabricators.snapfit.ui.main.artistlist.di.mainArtistUiModule
 import memory.fabricators.snapfit.ui.main.home.di.mainHomeUiModule
+import memory.fabricators.snapfit.ui.post.details.di.postUiModule
 import memory.fabricators.snapfit.ui.signup.di.signUpUiModule
 import memory.fabricators.snapfit.ui.start.di.startUiModule
 import org.koin.core.module.Module
@@ -14,5 +15,6 @@ val uiModule: Module
             startUiModule,
             mainArtistUiModule,
             mainHomeUiModule,
+            postUiModule,
         )
     }
