f430c3b9296fc4d135088ee72484c4c1f8858333
junsuPark <krpjs0508@gmail.com>
2024-08-26 21:04:21 +0900

    btn

--- app/src/main/java/memory/fabricators/snapfit/ui/post/details/PostDetailsScreen.kt
+++ app/src/main/java/memory/fabricators/snapfit/ui/post/details/PostDetailsScreen.kt
@@ -50,6 +50,7 @@ import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import memory.fabricators.snapfit.R
 import memory.fabricators.snapfit.core.design_system.BasicDialog
+import memory.fabricators.snapfit.core.design_system.Button
 import memory.fabricators.snapfit.core.design_system.LocalColorScheme
 import memory.fabricators.snapfit.core.design_system.LocalTypography
 import org.koin.androidx.compose.koinViewModel
@@ -141,203 +142,222 @@ fun PostDetailsScreen(
             )
         },
     ) { innerPaddings ->
-        Column(
+        Box(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPaddings)
-                .verticalScroll(rememberScrollState()),
+                .padding(innerPaddings),
+            contentAlignment = Alignment.BottomCenter,
         ) {
-            AnimatedVisibility(
-                visible = state.postDetails != null,
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .verticalScroll(rememberScrollState()),
             ) {
-                Column {
-                    val postDetails = state.postDetails!!
-                    val pagerState = rememberPagerState { postDetails.images.size }
-                    HorizontalPager(
-                        modifier = Modifier.fillMaxWidth(),
-                        state = pagerState,
-                        key = { state.postDetails!!.images[it] },
-                    ) { page ->
-                        val image = postDetails.images[page]
-                        AsyncImage(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .height(350.dp),
-                            // TODO: placeholder = painterResource(id = R.drawable.img_start_background),
-                            contentScale = ContentScale.Crop,
-                            model = image,
-                            contentDescription = null,
-                        )
-                    }
-                    Column(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(
-                                vertical = 32.dp,
-                            ),
-                    ) {
-                        LazyRow(
+                AnimatedVisibility(
+                    visible = state.postDetails != null,
+                ) {
+                    Column {
+                        val postDetails = state.postDetails!!
+                        val pagerState = rememberPagerState { postDetails.images.size }
+                        HorizontalPager(
+                            modifier = Modifier.fillMaxWidth(),
+                            state = pagerState,
+                            key = { state.postDetails!!.images[it] },
+                        ) { page ->
+                            val image = postDetails.images[page]
+                            AsyncImage(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .height(350.dp),
+                                // TODO: placeholder = painterResource(id = R.drawable.img_start_background),
+                                contentScale = ContentScale.Crop,
+                                model = image,
+                                contentDescription = null,
+                            )
+                        }
+                        Column(
                             modifier = Modifier
                                 .fillMaxWidth()
                                 .padding(
-                                    start = 16.dp,
+                                    vertical = 32.dp,
                                 ),
-                            horizontalArrangement = Arrangement.spacedBy(8.dp),
-                            verticalAlignment = Alignment.CenterVertically,
                         ) {
-                            items(postDetails.vibes) { vibe ->
-                                Tag {
-                                    Text(text = vibe)
+                            LazyRow(
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(
+                                        start = 16.dp,
+                                    ),
+                                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                                verticalAlignment = Alignment.CenterVertically,
+                            ) {
+                                items(postDetails.vibes) { vibe ->
+                                    Tag {
+                                        Text(text = vibe)
+                                    }
                                 }
                             }
-                        }
-                        Spacer(modifier = Modifier.height(16.dp))
-                        Text(
-                            text = postDetails.title,
-                            modifier = Modifier.padding(start = 16.dp),
-                            style = LocalTypography.current.title2Semibold,
-                            color = LocalColorScheme.current.primaryBlack,
-                        )
-                        Spacer(modifier = Modifier.height(16.dp))
-                        Row(
-                            modifier = Modifier.padding(
-                                start = 16.dp,
-                            ),
-                            horizontalArrangement = Arrangement.spacedBy(8.dp),
-                            verticalAlignment = Alignment.CenterVertically,
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.LocationOn,
-                                contentDescription = null,
-                                modifier = Modifier.size(24.dp),
-                                tint = LocalColorScheme.current.secondary400,
-                            )
+                            Spacer(modifier = Modifier.height(16.dp))
                             Text(
-                                text = postDetails.locations.joinToString(separator = " | "),
-                                style = LocalTypography.current.body2Regular,
-                                color = LocalColorScheme.current.secondary400,
+                                text = postDetails.title,
+                                modifier = Modifier.padding(start = 16.dp),
+                                style = LocalTypography.current.title2Semibold,
+                                color = LocalColorScheme.current.primaryBlack,
+                            )
+                            Spacer(modifier = Modifier.height(16.dp))
+                            Row(
+                                modifier = Modifier.padding(
+                                    start = 16.dp,
+                                ),
+                                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                                verticalAlignment = Alignment.CenterVertically,
+                            ) {
+                                Icon(
+                                    imageVector = Icons.Default.LocationOn,
+                                    contentDescription = null,
+                                    modifier = Modifier.size(24.dp),
+                                    tint = LocalColorScheme.current.secondary400,
+                                )
+                                Text(
+                                    text = postDetails.locations.joinToString(separator = " | "),
+                                    style = LocalTypography.current.body2Regular,
+                                    color = LocalColorScheme.current.secondary400,
+                                )
+                            }
+                            Spacer(modifier = Modifier.height(32.dp))
+                            Text(
+                                text = postDetails.prices.price.toString(),
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(start = 16.dp),
+                                style = LocalTypography.current.title1Semibold,
+                                color = LocalColorScheme.current.secondary500,
                             )
                         }
-                        Spacer(modifier = Modifier.height(32.dp))
-                        Text(
-                            text = postDetails.prices.price.toString(),
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(start = 16.dp),
-                            style = LocalTypography.current.title1Semibold,
-                            color = LocalColorScheme.current.secondary500,
-                        )
                     }
                 }
-            }
 
-            HorizontalDivider(
-                thickness = 5.dp,
-                color = LocalColorScheme.current.secondary100,
-            )
-            Row(
-                modifier = Modifier.padding(all = 16.dp),
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                AsyncImage(
-                    model = state.postDetails?.thumbnail ?: R.drawable.ic_launcher_background,
-                    contentDescription = null,
-                    modifier = Modifier
-                        .size(30.dp)
-                        .clip(
-                            shape = CircleShape,
-                        ),
-                    contentScale = ContentScale.Crop,
-                )
-                Text(
-                    text = state.postDetails?.maker?.nickname ?: "-",
-                    style = LocalTypography.current.body1Regular,
-                    color = LocalColorScheme.current.primaryBlack,
-                )
-            }
-            HorizontalDivider(
-                thickness = 5.dp,
-                color = LocalColorScheme.current.secondary100,
-            )
-            Column(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(
-                        vertical = 32.dp,
-                    ),
-            ) {
-                Text(
-                    text = "작가의 설명",
-                    modifier = Modifier.padding(
-                        horizontal = 16.dp,
-                    ),
-                    style = LocalTypography.current.subtitle1Semibold,
-                    color = LocalColorScheme.current.secondary500,
-                )
-                Spacer(modifier = Modifier.height(32.dp))
-                Text(
-                    text = state.postDetails?.desc ?: "-",
-                    modifier = Modifier.padding(
-                        horizontal = 16.dp,
-                    ),
-                    style = LocalTypography.current.caption2Regular,
-                    color = LocalColorScheme.current.secondary400,
-                )
-            }
-            HorizontalDivider(
-                thickness = 5.dp,
-                color = LocalColorScheme.current.secondary100,
-            )
-            Column(
-                modifier = Modifier.padding(vertical = 32.dp),
-            ) {
-                // TODO
-                Text(
-                    text = "작가의 등록된 상품",
-                    modifier = Modifier.padding(start = 16.dp),
-                    style = LocalTypography.current.subtitle1Semibold,
-                    color = LocalColorScheme.current.primaryBlack,
-                )
-                Spacer(modifier = Modifier.height(32.dp))
-                // TODO
-                val items = listOf(
-                    "123", "3215"
+                HorizontalDivider(
+                    thickness = 5.dp,
+                    color = LocalColorScheme.current.secondary100,
                 )
                 Row(
+                    modifier = Modifier.padding(all = 16.dp),
+                    horizontalArrangement = Arrangement.spacedBy(16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    AsyncImage(
+                        model = state.postDetails?.thumbnail ?: R.drawable.ic_launcher_background,
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(30.dp)
+                            .clip(
+                                shape = CircleShape,
+                            ),
+                        contentScale = ContentScale.Crop,
+                    )
+                    Text(
+                        text = state.postDetails?.maker?.nickname ?: "-",
+                        style = LocalTypography.current.body1Regular,
+                        color = LocalColorScheme.current.primaryBlack,
+                    )
+                }
+                HorizontalDivider(
+                    thickness = 5.dp,
+                    color = LocalColorScheme.current.secondary100,
+                )
+                Column(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(
+                            vertical = 32.dp,
+                        ),
+                ) {
+                    Text(
+                        text = "작가의 설명",
+                        modifier = Modifier.padding(
                             horizontal = 16.dp,
                         ),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                ) {
-                    items.forEach { item ->
-                        Box(
-                            modifier = Modifier
-                                .weight(1f)
-                                .height(300.dp)
-                                .background(
-                                    color = Color.Green,
-                                ),
-                        )
-                    }
+                        style = LocalTypography.current.subtitle1Semibold,
+                        color = LocalColorScheme.current.secondary500,
+                    )
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Text(
+                        text = state.postDetails?.desc ?: "-",
+                        modifier = Modifier.padding(
+                            horizontal = 16.dp,
+                        ),
+                        style = LocalTypography.current.caption2Regular,
+                        color = LocalColorScheme.current.secondary400,
+                    )
                 }
-                Spacer(modifier = Modifier.height(32.dp))
-                // TODO
-                Text(
-                    text = "취소 규정",
-                    modifier = Modifier.padding(start = 16.dp),
-                    style = LocalTypography.current.subtitle1Semibold,
-                    color = LocalColorScheme.current.primaryBlack,
+                HorizontalDivider(
+                    thickness = 5.dp,
+                    color = LocalColorScheme.current.secondary100,
                 )
-                Spacer(modifier = Modifier.height(32.dp))
-                Text(
-                    text = "가. 기본 환불 규정\n" + "1. 전문가와 의뢰인의 상호 협의하에 청약 철회 및 환불이 \n" + "   가능합니다.\n" + "2. 섭외, 대여 등 사전 준비 도중 청약 철회 시, 해당 비용을 공제한 \n" + "    금액을 환불 가능합니다.\n" + "3. 촬영 또는 편집 작업 착수 이후 청약 철회 시, 진행된 작업량 \n" + "    또는 작업 일수를 산정한 금액을 공제한 금액을 환불 가능합니다.",
-                    modifier = Modifier.padding(horizontal = 16.dp),
-                    style = LocalTypography.current.body2Regular,
-                    color = LocalColorScheme.current.secondary400,
-                )
+                Column(
+                    modifier = Modifier.padding(vertical = 32.dp),
+                ) {
+                    // TODO
+                    Text(
+                        text = "작가의 등록된 상품",
+                        modifier = Modifier.padding(start = 16.dp),
+                        style = LocalTypography.current.subtitle1Semibold,
+                        color = LocalColorScheme.current.primaryBlack,
+                    )
+                    Spacer(modifier = Modifier.height(32.dp))
+                    // TODO
+                    val items = listOf(
+                        "123", "3215"
+                    )
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(
+                                horizontal = 16.dp,
+                            ),
+                        horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    ) {
+                        items.forEach { item ->
+                            Box(
+                                modifier = Modifier
+                                    .weight(1f)
+                                    .height(300.dp)
+                                    .background(
+                                        color = Color.Green,
+                                    ),
+                            )
+                        }
+                    }
+                    Spacer(modifier = Modifier.height(32.dp))
+                    // TODO
+                    Text(
+                        text = "취소 규정",
+                        modifier = Modifier.padding(start = 16.dp),
+                        style = LocalTypography.current.subtitle1Semibold,
+                        color = LocalColorScheme.current.primaryBlack,
+                    )
+                    Spacer(modifier = Modifier.height(32.dp))
+                    Text(
+                        text = "가. 기본 환불 규정\n" + "1. 전문가와 의뢰인의 상호 협의하에 청약 철회 및 환불이 \n" + "   가능합니다.\n" + "2. 섭외, 대여 등 사전 준비 도중 청약 철회 시, 해당 비용을 공제한 \n" + "    금액을 환불 가능합니다.\n" + "3. 촬영 또는 편집 작업 착수 이후 청약 철회 시, 진행된 작업량 \n" + "    또는 작업 일수를 산정한 금액을 공제한 금액을 환불 가능합니다.",
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                        style = LocalTypography.current.body2Regular,
+                        color = LocalColorScheme.current.secondary400,
+                    )
+                }
+                Spacer(modifier = Modifier.height(128.dp))
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        start = 16.dp,
+                        end = 16.dp,
+                        bottom = 16.dp,
+                    ),
+            ) {
+                Text(text = "예약하기")
             }
         }
     }
