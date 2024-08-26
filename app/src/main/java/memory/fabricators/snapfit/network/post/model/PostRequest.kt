93867419baaa50556bd34d7c1187d1b71097cb20
junsuPark <krpjs0508@gmail.com>
2024-08-26 20:49:27 +0900

    req

--- app/src/main/java/memory/fabricators/snapfit/network/post/model/PostRequest.kt
+++ app/src/main/java/memory/fabricators/snapfit/network/post/model/PostRequest.kt
@@ -11,7 +11,7 @@ data class PostRequest(
     @SerialName("thumbnail") val thumbnail: String,
     val title: String,
     val desc: String,
-    val prices: List<PriceRequest>,
+    val prices: PriceRequest,
     val personPrice: Long,
     val studio: Boolean,
 ) {
