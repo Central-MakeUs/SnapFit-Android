a98ee94a6e4f1f001736163bca27706dcaa4bb9a
junsuPark <krpjs0508@gmail.com>
2024-08-27 18:58:58 +0900

    post mod

---
+++ app/src/main/java/memory/fabricators/snapfit/ui/post/booking/di/PostUiModule.kt
@@ -0,0 +1,11 @@
+package memory.fabricators.snapfit.ui.post.booking.di
+
+import memory.fabricators.snapfit.ui.post.booking.BookingViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.core.module.Module
+import org.koin.dsl.module
+
+val postBookingUiModule: Module
+    get() = module {
+        viewModel { BookingViewModel() }
+    }
