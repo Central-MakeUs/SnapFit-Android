2e71e54f71baab64088ee20a476ac47d2d2d2259
junsuPark <krpjs0508@gmail.com>
2024-08-27 19:33:47 +0900

    booking

--- app/src/main/java/memory/fabricators/snapfit/ui/post/booking/BookingScreen.kt
+++ app/src/main/java/memory/fabricators/snapfit/ui/post/booking/BookingScreen.kt
@@ -1,10 +1,14 @@
 package memory.fabricators.snapfit.ui.post.booking
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -12,17 +16,22 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import memory.fabricators.snapfit.R
 import memory.fabricators.snapfit.core.design_system.LocalColorScheme
+import memory.fabricators.snapfit.core.design_system.LocalTypography
 import memory.fabricators.snapfit.core.design_system.SectionHeader
 import org.koin.androidx.compose.koinViewModel
+import org.orbitmvi.orbit.compose.collectAsState
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,12 +41,18 @@ fun BookingScreen(
     modifier: Modifier = Modifier,
     viewModel: BookingViewModel = koinViewModel(),
 ) {
+    val state by viewModel.collectAsState()
+
     val (time, onChangeTime) = remember { mutableStateOf("") }
     val (preferLocation, onChangePreferLocation) = remember { mutableStateOf("") }
     val (preferTime, onChangePreferTime) = remember { mutableStateOf("") }
     val (countOfPeople, onChangeCountOfPeople) = remember { mutableIntStateOf(0) }
     val (email, onChangeEmail) = remember { mutableStateOf("") }
     val (phoneNumber, onChangePhoneNumber) = remember { mutableStateOf("") }
+
+    LaunchedEffect(key1 = postId) {
+        viewModel.fetchPostDetails(postId)
+    }
 
     Scaffold(
         modifier = modifier,
@@ -71,14 +86,45 @@ fun BookingScreen(
                 SectionHeader(
                     title = { Text(text = "주문상품") },
                 )
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
+                AnimatedVisibility(
+                    visible = state.postDetails != null,
                 ) {
-                    // AsyncImage(model = , contentDescription = )
-                    /*AsyncImage(
-                        model =,
-                        contentDescription =,
-                    )*/
+                    val postDetails = state.postDetails!!
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                    ) {
+                        AsyncImage(
+                            model = postDetails.thumbnail,
+                            contentDescription = postDetails.title,
+                            modifier = Modifier
+                                .size(
+                                    size = 130.dp,
+                                )
+                                .clip(
+                                    shape = RoundedCornerShape(5.dp),
+                                ),
+                        )
+                        Column(
+                            modifier = Modifier.weight(1f),
+                            verticalArrangement = Arrangement.spacedBy(4.dp),
+                        ) {
+                            Text(
+                                text = postDetails.title,
+                                style = LocalTypography.current.body2Semibold,
+                                color = LocalColorScheme.current.secondary500,
+                            )
+                            Text(
+                                text = postDetails.locations.joinToString(),
+                                style = LocalTypography.current.caption2Regular,
+                                color = LocalColorScheme.current.secondary500,
+                            )
+                            Text(
+                                text = postDetails.prices.price.toString(),
+                                style = LocalTypography.current.caption1Semibold,
+                                color = LocalColorScheme.current.secondary500,
+                            )
+                        }
+                    }
                 }
             }
         }
--- app/src/main/java/memory/fabricators/snapfit/ui/post/booking/BookingViewModel.kt
+++ app/src/main/java/memory/fabricators/snapfit/ui/post/booking/BookingViewModel.kt
@@ -1,13 +1,34 @@
 package memory.fabricators.snapfit.ui.post.booking
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import memory.fabricators.snapfit.data.post.PostRepository
 import memory.fabricators.snapfit.data.post.model.PostDetails
 import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.reduce
 import org.orbitmvi.orbit.viewmodel.container
 
-class BookingViewModel : ViewModel(),
-    ContainerHost<BookingState, Unit> {
+class BookingViewModel(
+    private val postRepository: PostRepository,
+) : ViewModel(), ContainerHost<BookingState, Unit> {
     override val container = container<BookingState, Unit>(BookingState())
+
+    fun fetchPostDetails(postId: Long) = intent {
+        viewModelScope.launch(Dispatchers.IO) {
+            kotlin.runCatching {
+                postRepository.getPostDetails(postId)
+            }.onSuccess {
+                reduce {
+                    state.copy(postDetails = it)
+                }
+            }.onFailure {
+                it.printStackTrace()
+            }
+        }
+    }
 }
 
 data class BookingState(
