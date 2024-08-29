3d55f74459247121edcc282566f741f281af781f
junsuPark <krpjs0508@gmail.com>
2024-08-29 21:51:21 +0900

    vm

--- app/src/main/java/memory/fabricators/snapfit/ui/reservation/list/ReservationListViewModel.kt
+++ app/src/main/java/memory/fabricators/snapfit/ui/reservation/list/ReservationListViewModel.kt
@@ -1,13 +1,44 @@
 package memory.fabricators.snapfit.ui.reservation.list
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import memory.fabricators.snapfit.data.reservation.ReservationRepository
+import memory.fabricators.snapfit.data.reservation.model.ReservationList
 import org.orbitmvi.orbit.ContainerHost
+import org.orbitmvi.orbit.syntax.simple.intent
+import org.orbitmvi.orbit.syntax.simple.reduce
+import org.orbitmvi.orbit.viewmodel.container
 
-class ReservationListViewModel: ViewModel(),
-        ContainerHost<> {
+class ReservationListViewModel(
+    private val reservationRepository: ReservationRepository,
+) : ViewModel(),
+    ContainerHost<ReservationListState, Unit> {
+    override val container = container<ReservationListState, Unit>(ReservationListState())
 
+    init {
+        fetchReservationList()
+    }
+
+    private fun fetchReservationList() = intent {
+        viewModelScope.launch(Dispatchers.IO) {
+            kotlin.runCatching {
+                reservationRepository.getReservations(
+                    offset = 0,
+                    limit = 20,
+                )
+            }.onSuccess {
+                reduce {
+                    state.copy(reservationList = it.data)
+                }
+            }.onFailure {
+                it.printStackTrace()
+            }
         }
+    }
+}
 
 data class ReservationListState(
-    val reservationList: List
+    val reservationList: List<ReservationList.ReservationListItem>? = null,
 )
