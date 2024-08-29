ce742c30250c6822ada77be893387ad63188a53c
junsuPark <krpjs0508@gmail.com>
2024-08-29 21:41:17 +0900

    details

---
+++ app/src/main/java/memory/fabricators/snapfit/ui/reservation/details/ReservationDetailsScreen.kt
@@ -0,0 +1,13 @@
+package memory.fabricators.snapfit.ui.reservation.details
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+fun ReservationDetailsScreen(
+    modifier: Modifier = Modifier,
+    viewModel: ReservationDetailsViewModel = koinViewModel(),
+) {
+
+}
