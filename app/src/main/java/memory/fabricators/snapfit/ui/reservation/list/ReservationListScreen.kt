f11449ccb8a1f5696f47e9d43f53dbb3535dfe6b
junsuPark <krpjs0508@gmail.com>
2024-08-29 21:41:14 +0900

    ls

---
+++ app/src/main/java/memory/fabricators/snapfit/ui/reservation/list/ReservationListScreen.kt
@@ -0,0 +1,57 @@
+package memory.fabricators.snapfit.ui.reservation.list
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import memory.fabricators.snapfit.R
+import memory.fabricators.snapfit.core.design_system.LocalColorScheme
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ReservationListScreen(
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+
+) {
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "예약 내역") },
+                navigationIcon = {
+                    IconButton(
+                        onClick = onNavigateUp,
+                    ) {
+                        Icon(
+                            tint = LocalColorScheme.current.primaryBlack,
+                            painter = painterResource(id = R.drawable.icon_arrow_left),
+                            contentDescription = "back",
+                        )
+                    }
+                },
+            )
+        },
+    ) { innerPaddings ->
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .fillMaxSize()
+                .padding(innerPaddings)
+                .imePadding(),
+        ) {
+
+        }
+    }
+}
